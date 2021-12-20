4455eb76807d2e2a926a82e8221f6fb177273109
Sanowar Dewan <sanowardwnofficial@gmail.com>
2021-12-20 23:08:28 +0600

    view user done

--- users/user-add.php
+++ users/user-add.php
@@ -9,30 +9,38 @@ require '../dashboard_includes/header.php'; ?>
             </div>
             <div class="tab-inn">
                 <form action="registration_post.php" method="POST"> 
+                    <?php if(isset($_SESSION['register_success'])){?>
+                        <div class="alert alert-success">
+                            <?= $_SESSION['register_success'] ?>
+                        </div>
+                   <?php } unset($_SESSION['register_success'])?>  
                     <div class="row">
                         <div class="input-field col s12">
-                            <input name="name" type="text">
-                            <?php if(isset($_SESSION['blank_error']['name'])){
-                                echo $_SESSION['blank_error']['name'];
-                            } unset($_SESSION['blank_error']['name']) ?>
+                            <input value="<?= isset($_SESSION['name'])?$_SESSION['name']:''; ?>" name="name" type="text">
+                            <?php if(isset($_SESSION['blank_error']['name'])){ ?>
+                                <div class="alert alert-warning"> <?= $_SESSION['blank_error']['name']; ?> </div>
+                                <?php  } unset($_SESSION['blank_error']['name']) ?>
                             <label for="first_name">Full Name</label>
                         </div>
                     </div>
                     <div class="row">
                         <div class="input-field col s12">
-                            <input name="email" type="email">
-                            <?php if(isset($_SESSION['blank_error']['email'])){
-                                echo $_SESSION['blank_error']['email'];
-                            } unset($_SESSION['blank_error']['email']) ?>
+                            <input value="<?= isset($_SESSION['email'])?$_SESSION['email']:''; ?>" name="email" type="email">
+                            <?php if(isset($_SESSION['blank_error']['email'])){ ?>
+                                <div class="alert alert-warning"> <?= $_SESSION['blank_error']['email']; ?> </div>
+                                <?php  } unset($_SESSION['email']) ?>
+                            <?php if(isset($_SESSION['exist_email'])){ ?>
+                                <div class="alert alert-warning"> <?= $_SESSION['exist_email']; ?> </div>
+                                <?php  } unset($_SESSION['exist_email']) ?>
                             <label for="email">Email</label>
                         </div>
                     </div>
                     <div class="row">
                         <div class="input-field col s12">
-                            <input name="password" type="password">
-                            <?php if(isset($_SESSION['blank_error']['password'])){
-                                echo $_SESSION['blank_error']['password'];
-                            } unset($_SESSION['blank_error']['password']) ?>
+                            <input value="<?= isset($_SESSION['password'])?$_SESSION['password']:''; ?>" name="password" type="password">
+                            <?php if(isset($_SESSION['blank_error']['password'])){ ?>
+                                <div class="alert alert-warning"> <?= $_SESSION['blank_error']['password']; ?> </div>
+                                <?php  } unset($_SESSION['blank_error']['password']) ?>
                             <label for="password">Password</label>
                         </div>
                     </div>
@@ -44,5 +52,9 @@ require '../dashboard_includes/header.php'; ?>
                 </form>
             </div>
         </div>
-                        
+<?php 
+unset($_SESSION['name']);
+unset($_SESSION['email']);
+unset($_SESSION['password']);
+?>
 <?php require '../dashboard_includes/footer.php'; ?> 

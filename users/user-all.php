cc0c8b007571a1a5f80a5117b362f43c41dea7ae
Identity Office Dev <sanowardwnofficial@gmail.com>
2021-12-21 15:24:54 +0600

    edit user done

--- users/user-all.php
+++ users/user-all.php
@@ -1,92 +1,93 @@
-<?php require '../dashboard_includes/header.php'; require '../db.php'; ?>
+<?php require '../dashboard_includes/header.php';
+require '../db.php'; ?>
 <?php
 $select_user = "SELECT * FROM users";
 $select_user_result = mysqli_query($db_connect, $select_user);
 ?>
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="box-inn-sp">
-                                <div class="inn-title">
-                                    <h4>User Details</h4>
-                                    <a class="dropdown-button drop-down-meta" href="#" data-activates="dr-users"><i class="material-icons">more_vert</i></a>
-                                    <ul id="dr-users" class="dropdown-content">
-                                        <li><a href="user-add.html">Add New</a>
-                                        </li>
-                                        <li><a href="user-edit.html">Edit</a>
-                                        </li>
-                                        <li><a href="#!">Update</a>
-                                        </li>
-                                        <li class="divider"></li>
-                                        <li><a href="#!"><i class="material-icons">delete</i>Delete</a>
-                                        </li>
-                                        <li><a href="user-view.html"><i class="material-icons">subject</i>View All</a>
-                                        </li>
-                                    </ul>
+<div class="row">
+    <div class="col-md-12">
+        <div class="box-inn-sp">
+            <div class="inn-title">
+                <h4>User Details</h4>
+                <a class="dropdown-button drop-down-meta" href="#" data-activates="dr-users"><i class="material-icons">more_vert</i></a>
+                <ul id="dr-users" class="dropdown-content">
+                    <li><a href="user-add.html">Add New</a>
+                    </li>
+                    <li><a href="user-edit.html">Edit</a>
+                    </li>
+                    <li><a href="#!">Update</a>
+                    </li>
+                    <li class="divider"></li>
+                    <li><a href="#!"><i class="material-icons">delete</i>Delete</a>
+                    </li>
+                    <li><a href="user-view.html"><i class="material-icons">subject</i>View All</a>
+                    </li>
+                </ul>
 
-                                    <!-- Dropdown Structure -->
+                <!-- Dropdown Structure -->
 
-                                </div>
-                                <div class="tab-inn">
-                                    <div class="table-responsive table-desi">
-                                        <table class="table table-hover">
-                                            <thead>
-                                                <tr>
-                                                    <th>User</th>
-                                                    <th>Name</th>
-                                                    <th>Email</th>
-                                                    <th>View</th>
-                                                    <th>Edit</th>
-                                                    <th>Delete</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <?php foreach($select_user_result as $key => $user){ ?>
-                                                <tr>
-                                                    <td><span class="list-img"><img src="images/user/1.png" alt=""></span>
-                                                    </td>
-                                                    <td><a href="#"><span class="list-enq-name"><?= $user['name']; ?></span></a>
-                                                    </td>
-                                                    <td><?= $user['email']; ?></td>
-                                                    <td>
-                                                        <a href="user-view.php?id=<?= $user['id']; ?>"><i class="fa fa-eye" aria-hidden="true"></i></a>
-                                                    </td>
-                                                    <td>
-                                                        <a href="user-edit.html"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
-                                                    </td>
-                                                    <td>
-                                                        <a href="#"><i class="fa fa-trash-o" aria-hidden="true"></i></a>
-                                                    </td>
-                                                </tr>
-                                                <?php } ?>
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+            </div>
+            <div class="tab-inn">
+                <div class="table-responsive table-desi">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th>User</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>View</th>
+                                <th>Edit</th>
+                                <th>Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($select_user_result as $key => $user) { ?>
+                                <tr>
+                                    <td><span class="list-img"><img src="images/user/1.png" alt=""></span>
+                                    </td>
+                                    <td><a href="#"><span class="list-enq-name"><?= $user['name']; ?></span></a>
+                                    </td>
+                                    <td><?= $user['email']; ?></td>
+                                    <td>
+                                        <a href="user-view.php?id=<?= $user['id']; ?>"><i class="fa fa-eye" aria-hidden="true"></i></a>
+                                    </td>
+                                    <td>
+                                        <a href="user-edit.php?id=<?= $user['id']; ?>"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
+                                    </td>
+                                    <td>
+                                        <a href="#"><i class="fa fa-trash-o" aria-hidden="true"></i></a>
+                                    </td>
+                                </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
     </div>
+</div>
+</div>
+</div>
+</div>
+</div>
 
-    <!--== BOTTOM FLOAT ICON ==-->
-    <section>
-        <div class="fixed-action-btn vertical">
-            <a class="btn-floating btn-large red pulse">
-                <i class="large material-icons">mode_edit</i>
-            </a>
-            <ul>
-                <li><a class="btn-floating red"><i class="material-icons">insert_chart</i></a>
-                </li>
-                <li><a class="btn-floating yellow darken-1"><i class="material-icons">format_quote</i></a>
-                </li>
-                <li><a class="btn-floating green"><i class="material-icons">publish</i></a>
-                </li>
-                <li><a class="btn-floating blue"><i class="material-icons">attach_file</i></a>
-                </li>
-            </ul>
-        </div>
-    </section>
+<!--== BOTTOM FLOAT ICON ==-->
+<section>
+    <div class="fixed-action-btn vertical">
+        <a class="btn-floating btn-large red pulse">
+            <i class="large material-icons">mode_edit</i>
+        </a>
+        <ul>
+            <li><a class="btn-floating red"><i class="material-icons">insert_chart</i></a>
+            </li>
+            <li><a class="btn-floating yellow darken-1"><i class="material-icons">format_quote</i></a>
+            </li>
+            <li><a class="btn-floating green"><i class="material-icons">publish</i></a>
+            </li>
+            <li><a class="btn-floating blue"><i class="material-icons">attach_file</i></a>
+            </li>
+        </ul>
+    </div>
+</section>
 
 <?php require '../dashboard_includes/footer.php' ?>
--- users/user-edit.php
+++ users/user-edit.php
@@ -1,60 +1,68 @@
-<?php require '../dashboard_includes/header.php'; require '../db.php'; ?>
+<?php require '../dashboard_includes/header.php';
+require '../db.php'; ?>
+<?php
+$id = $_GET['id'];
+$select_user = "SELECT * FROM users WHERE id = $id";
+$select_user_result = mysqli_query($db_connect, $select_user);
+$after_assoc = mysqli_fetch_assoc($select_user_result);
+?>
 
+<div class="row">
+    <div class="col-md-12">
+        <div class="box-inn-sp">
+            <div class="inn-title">
+                <h4>Edit User Details</h4>
+            </div>
+            <div class="tab-inn">
+                <form action="edit_user_post.php" method="POST">
                     <div class="row">
-                        <div class="col-md-12">
-                            <div class="box-inn-sp">
-                                <div class="inn-title">
-                                    <h4>Edit User Details</h4>
-                                </div>
-                                <div class="tab-inn">
-                                    <form>
-                                        <div class="row">
-                                            <div class="input-field col s12">
-                                                <input id="first_name" type="text" value="Marsha" class="validate">
-                                                <label for="first_name">Full Name</label>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="input-field col s6">
-                                                <input id="password" type="password" value="aksdjfhasdf" class="validate">
-                                                <label for="password">Password</label>
-                                            </div>
-                                            <div class="input-field col s6">
-                                                <input id="email" type="email" value="marshahi@mail.com" class="validate">
-                                                <label for="email">Email</label>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="input-field col s12">
-                                                <input type="submit" class="waves-effect waves-light btn-large">
-                                            </div>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
+                        <div class="input-field col s12">
+                            <input value="<?= $after_assoc['id'] ?>" name="id" type="hidden">
+                            <input name="name" id="first_name" type="text" value="<?= $after_assoc['name'] ?>">
+                            <label for="first_name">Full Name</label>
                         </div>
                     </div>
-                </div>
+                    <div class="row">
+                        <div class="input-field col s6">
+                            <input id="password" type="password" name="password" value="<?= $after_assoc['password'] ?>">
+                            <label for="password">Password</label>
+                        </div>
+                        <div class="input-field col s6">
+                            <input id="email" type="email" name="email" value="<?= $after_assoc['email'] ?>">
+                            <label for="email">Email</label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <input type="submit" class="waves-effect waves-light btn-large">
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
+</div>
+</div>
+</div>
+</div>
+</div>
 
-    <!--== BOTTOM FLOAT ICON ==-->
-    <section>
-        <div class="fixed-action-btn vertical">
-            <a class="btn-floating btn-large red pulse">
-                <i class="large material-icons">mode_edit</i>
-            </a>
-            <ul>
-                <li><a class="btn-floating red"><i class="material-icons">insert_chart</i></a>
-                </li>
-                <li><a class="btn-floating yellow darken-1"><i class="material-icons">format_quote</i></a>
-                </li>
-                <li><a class="btn-floating green"><i class="material-icons">publish</i></a>
-                </li>
-                <li><a class="btn-floating blue"><i class="material-icons">attach_file</i></a>
-                </li>
-            </ul>
-        </div>
-    </section>
-<?php require '../dashboard_includes/footer.php';?>
+<!--== BOTTOM FLOAT ICON ==-->
+<section>
+    <div class="fixed-action-btn vertical">
+        <a class="btn-floating btn-large red pulse">
+            <i class="large material-icons">mode_edit</i>
+        </a>
+        <ul>
+            <li><a class="btn-floating red"><i class="material-icons">insert_chart</i></a>
+            </li>
+            <li><a class="btn-floating yellow darken-1"><i class="material-icons">format_quote</i></a>
+            </li>
+            <li><a class="btn-floating green"><i class="material-icons">publish</i></a>
+            </li>
+            <li><a class="btn-floating blue"><i class="material-icons">attach_file</i></a>
+            </li>
+        </ul>
+    </div>
+</section>
+<?php require '../dashboard_includes/footer.php'; ?>

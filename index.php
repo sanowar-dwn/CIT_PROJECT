80ded27d4b2b67bb70a53dabdd907e7b975346eb
Identity Office Dev <sanowardwnofficial@gmail.com>
2021-12-30 10:33:03 +0600

    banner dynamic

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+require 'db.php';
+
+$select_banner = "SELECT * FROM banners";
+$select_banner_result = mysqli_query($db_connect, $select_banner);
+$after_assoc_banner = mysqli_fetch_assoc($select_banner_result);
+?>
 <!doctype html>
 <html class="no-js" lang="en">
 
@@ -120,18 +127,18 @@
                     <div class="row align-items-center">
                         <div class="col-xl-7 col-lg-6">
                             <div class="banner-content">
-                                <h6 class="wow fadeInUp" data-wow-delay="0.2s">HELLO!</h6>
-                                <h2 class="wow fadeInUp" data-wow-delay="0.4s">I am Will Smith</h2>
-                                <p class="wow fadeInUp" data-wow-delay="0.6s">I'm Will Smith, professional web developer with long time experience in this field​.</p>
+                                <h6 class="wow fadeInUp" data-wow-delay="0.2s"><?= $after_assoc_banner['intro']; ?></h6>
+                                <h2 class="wow fadeInUp" data-wow-delay="0.4s">I am <?= $after_assoc_banner['name'] ?></h2>
+                                <p class="wow fadeInUp" data-wow-delay="0.6s"><?= $after_assoc_banner['descrip'] ?></p>
                                 <div class="banner-social wow fadeInUp" data-wow-delay="0.8s">
                                     <ul>
-                                        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                                        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                                        <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-                                        <li><a href="#"><i class="fab fa-pinterest"></i></a></li>
+                                        <li><a href="<?= $after_assoc_banner['link1'] ?>"><i class="fab fa-facebook-f"></i></a></li>
+                                        <li><a href="<?= $after_assoc_banner['link2'] ?>"><i class="fab fa-twitter"></i></a></li>
+                                        <li><a href="<?= $after_assoc_banner['link3'] ?>"><i class="fab fa-instagram"></i></a></li>
+                                        <li><a href="<?= $after_assoc_banner['link4'] ?>"><i class="fab fa-pinterest"></i></a></li>
                                     </ul>
                                 </div>
-                                <a href="#" class="btn wow fadeInUp" data-wow-delay="1s">SEE PORTFOLIOS</a>
+                                <a href="#" class="btn wow fadeInUp" data-wow-delay="1s"><?= $after_assoc_banner['button'] ?> </a>
                             </div>
                         </div>
                         <div class="col-xl-5 col-lg-6 d-none d-lg-block">

ef91295ad656ada4320672a311184632ac5dba56
Sanowar Dewan <sanowardwnofficial@gmail.com>
2023-07-07 12:28:10 +0600

    edits

---
+++ readme.md
@@ -0,0 +1,7 @@
+# ERC-20 Token - LogiCoin
+
+This is a basic implementation of the ERC-20 standard.
+
+Contract is deployed on the Sepolia Test net.
+
+Import this "0xF098E992C4D51E6937CC66E10ca9d7509cc537Aa" address to your MetaMask wallet.
--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,6 +1,7 @@
-from brownie import accounts, LogiCoin
+from brownie import accounts, LogiCoin, config
 import time
+
 def main():
-    account = accounts[0]
-    contract_address = LogiCoin.deploy({'from':account})
-    time.sleep(1)
+    account = accounts.add(config['wallets']['from_key'])
+    contract_address = LogiCoin.deploy({'from':account}, publish_source=True)
+    time.sleep(1) # This line is used to avoid the Web3 package bug
